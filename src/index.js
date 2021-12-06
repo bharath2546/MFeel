608b34e09fba9d5f29c821b41f0c9ea1f1ebc6d5
bharath2546 <bharath2546@gmail.com>
2021-12-06 14:01:23 +0530

    added frontend and server section

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>hello this is a Mfeel Website</h1>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
